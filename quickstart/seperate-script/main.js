bce362c3091524d9692a3c1597b6fba03e25b9fe
Jakob Waibel <jakob.waibel@web.de>
2020-10-31 11:23:47 +0100

    feat: Add seperate script file

---
+++ quickstart/seperate-script/main.js
@@ -0,0 +1,5 @@
+const makeBig = () => {document.getElementById('paragraph').style = 'font-weight: bold'}
+const makeRed = () => {document.getElementById('paragraph').style = 'color: red'}
+const makeCat = () => {document.getElementById('cat').src = 'https://http.cat/404'}
+const makeCatGone = () => {document.getElementById('cat').style = 'display: none'}
+const makeBigger = () => {document.getElementById('paragraph').style = 'font-size: 32px'}
