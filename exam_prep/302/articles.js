a5c3957faf6c09b93dbab74ac9e0991d452e5fed
Jakob Waibel <jakob.waibel@web.de>
2021-01-20 12:17:24 +0100

    feat: Add C

---
+++ exam_prep/302/articles.js
@@ -0,0 +1,11 @@
+const articles = [
+	{ type: 'tablet',     name: 'ASUS MeMO Pad Smart',             price: 350, display: '10.1", 1280x800', os: 'Android 4.1',   battery: 'Li-Polymer, 5070mAh' },
+	{ type: 'tablet',     name: 'Samsung Galaxy Tab Active2 T395', price: 450, display: '8", 1280x800',    os: 'Android 7.1',   battery: 'Li-Ionen, 4450mAh', connection: '4G, 3G, 2G' },
+	{ type: 'tablet',     name: 'Apple iPad',                      price: 400, display: '9.7", 2048x1536', os: 'iPadOS 13.2.3', battery: 'Li-Polymer, 8827mAh' },
+	
+	{ type: 'pc',         name: 'Workstation',                     price: 1200, cpu: 'Ryzen 7 1700', gpu: 'Radeon RX 5700', memory: '16GB' },
+	
+	{ type: 'notebook',   name: 'ASUS TUF Gaming FX504GD-EN385T',  price: 850, cpu: 'Intel Core i5-8300H', gpu: 'NVIDIA GeForce GTX 1050 (Mobile)',     memory: '8GB',  display: '15.6", 1920x1080' },
+	{ type: 'notebook',   name: 'Lenovo ThinkPad E595',            price: 850, cpu: 'AMD Ryzen 7 3700U',   gpu: 'AMD Radeon RX Vega 10 Graphics (IGP)', memory: '16GB', display: '15.6", 1920x1080' },
+	{ type: 'notebook',   name: 'Apple MacBook Air',               price: 950, cpu: 'Intel Core i5-5350U', gpu: 'Intel HD Graphics 6000 (IGP)',         memory: '8GB',  display: '13.3", 1440x900' }
+]
